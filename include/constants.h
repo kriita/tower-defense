e4c82aa660f62781a587a5a291a9ecca658b32b7
Filip Jaredson <filja057@student.liu.se>
2018-12-05 14:52:35 +0100

    Standard font sizes similar to CSS added

--- include/constants.h
+++ include/constants.h
@@ -37,6 +37,18 @@ float const mapBorderOffset {12};
 
 float const sidebarPosX {mapBorderOffset*2 + xTilesMax*tileWidth};
 
+/**
+ * Font Size
+ */
+
+int const EM {16};
+
+int const H1 {static_cast<int>(3 * EM)};
+int const H2 {static_cast<int>(2.25 * EM)};
+int const H3 {static_cast<int>(1.75 * EM)};
+int const H4 {static_cast<int>(1.125 * EM)};
+int const BODY {static_cast<int>(EM)};
+
 /*
  * Map data characters
  */

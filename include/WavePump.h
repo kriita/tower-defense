a80d300afcbc453d5ff27765f3d8a624785abaad
Philip Pettersson <phipe378@student.liu.se>
2018-12-11 04:33:31 +0100

    'some' progress on WavePump

--- include/WavePump.h
+++ include/WavePump.h
@@ -10,13 +10,22 @@
 #include <iostream>
 
 //notes: vore trevligt att ha monsterVectorn som en pekare här.
+//trevligt också om monster.getType var virutal kom inte åt den i ett test
 
 class WavePump
 {
 public:
     WavePump();
     ~WavePump() = default;
-    void setSpawnTile(shptr<Tile>);
+    //void setSpawnTile(shptr<Tile>);
+    void addMonsterType(shptr<Monster>);
+    void testing() 
+    {
+	for (unsigned int i{0}; i < monsterTypes.size(); i++)
+	{
+	    std::cout << monsterTypes[i] << std::endl;
+	}
+    }
     
 private:
     int waveCount{};
@@ -27,7 +36,7 @@ private:
     std::vector<shptr<Monster>> monsterTypes{};
     std::vector<shptr<Monster>> monsterSequence{};
     int monsterSequenceIndex{};
-    shptr<Tile> spawnTile{};
+    //shptr<Tile> spawnTile{};
     
 };
 
--- src/WavePump.cpp
+++ src/WavePump.cpp
@@ -2,11 +2,18 @@
 
 WavePump::WavePump()
     :waveCount{1}, monsterLevelRoof{5}, monsterLevelFloor{1}, 
-    spawnCooldown{0.5f}, clock{}, monsterTypes{}, monsterSequence{},
-    monsterSequenceIndex{0}, spawnTile{nullptr}
-{}
+    spawnCooldown{0.5f}, clock{}, monsterTypes{}, monsterSequence{7},
+    monsterSequenceIndex{0}//, spawnTile{nullptr}
+{
 
-void WavePump::setSpawnTile(shptr<Tile> tile)
+}
+
+/*void WavePump::setSpawnTile(shptr<Tile> tile)
 {
     spawnTile = std::move(tile);
+}*/
+
+void WavePump::addMonsterType(shptr<Monster> monster)
+{
+    monsterTypes.push_back(monster);
 }

1d59afa73cf2c0b2428c57ec884e1ab0f939fca9
Henrik Holmberg <henho614@student.liu.se>
2018-12-15 18:19:53 +0100

    Added 2 maps, added {} to tower texture init

--- src/test/map_test.cpp
+++ src/test/map_test.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 TEST_CASE("Normal map")
 {
-    Map m {"test/map.dat"};
+    Map m {"test/map"};
 
     CHECK(m.getTile(1, 1)->getTileX() == 1);
     CHECK(m.getTile(1, 1)->getTileY() == 1);
@@ -39,20 +39,20 @@ TEST_CASE("Normal map")
 TEST_CASE("Error throws")
 {
     // Incorrect map file name
-    REQUIRE_THROWS_AS(make_unique<Map>("239867230476slodhnirgsldfhndsjk.dat"), MapError);
+    REQUIRE_THROWS_AS(make_unique<Map>("239867230476slodhnirgsldfhndsjk"), MapError);
 
     // Incorrect line length
-    REQUIRE_THROWS_AS(make_unique<Map>("test/1.dat"), MapError);
+    REQUIRE_THROWS_AS(make_unique<Map>("test/1"), MapError);
 
     // No spawn point
-    REQUIRE_THROWS_AS(make_unique<Map>("test/2.dat"), MapError);
+    REQUIRE_THROWS_AS(make_unique<Map>("test/2"), MapError);
 
     // No end point
-    REQUIRE_THROWS_AS(make_unique<Map>("test/3.dat"), MapError);
+    REQUIRE_THROWS_AS(make_unique<Map>("test/3"), MapError);
 
     // Multiple spawn points
-    REQUIRE_THROWS_AS(make_unique<Map>("test/4.dat"), MapError);
+    REQUIRE_THROWS_AS(make_unique<Map>("test/4"), MapError);
 
     // Multiple end points
-    REQUIRE_THROWS_AS(make_unique<Map>("test/5.dat"), MapError);
+    REQUIRE_THROWS_AS(make_unique<Map>("test/5"), MapError);
 }
